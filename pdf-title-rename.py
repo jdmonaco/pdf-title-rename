e37eedce7ee164e911fddd971ef3706c812369c8
Joe Monaco <self@joemona.co>
2014-08-11 19:57:52 -0400

    Working draft, Author - Title.pdf format

--- pdf-title-rename.py
+++ pdf-title-rename.py
@@ -1,43 +1,119 @@
 #!/usr/bin/env python
 
 """
-script.py -- A script to perform some job
-
-Usage: script.py [args]
+A script to batch rename pdfs based on title
 """
 
 from __future__ import print_function, division
 
 import os
-import re
 import sys
 import argparse
 import subprocess
-# import numpy as np
-# import scipy as sp
 
-# Package imports
-
-# External imports
-
-# Constants
+# PDF and metadata libraries
+from pdfminer.pdfparser import PDFParser, PDFSyntaxError
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdftypes import resolve1
+from xmp import xmp_to_dict
 
 
-class Foobar(object):
+class RenamePDFsByTitle(object):
 
     """
-    This class runs the script
+    This class parses PDF files for title and author and then
+    renames them.
     """
 
     def __init__(self, args):
-        self.args = args
+        self.pdf_files = args.files
+        self.dry_run = args.dry_run
 
     def main(self):
         """Entry point for running the script."""
-        print(*self.args)
+        for f in self.pdf_files:
+            path, base = os.path.split(f)
+            title, author = self._get_info(f)
+            if title:
+                g = os.path.join(path, self._new_filename(base, title, author))
+                print('moving', '\"%s\"' % f, 'to', '\"%s\"' % g)
+                if not self.dry_run:
+                    os.rename(f, g)
         return 0
+
+    def _new_filename(self, oldfn, title, author):
+        n = self._sanitize(title)
+        if author:
+            n = '%s - %s' % (self._sanitize(author), n)
+        n = '%s.pdf' % n[:250]  # limit filenames to ~255 chars
+        return n
+
+    def _sanitize(self, s):
+        keep = [' ', '.', '_', '-', u'\u2014']
+        return "".join(c for c in s if c.isalnum() or c in keep).strip()
+
+    def _get_info(self, filename):
+        title = author = None
+        with file(filename, 'rb') as pdf:
+            info = self._get_metadata(pdf)
+            if not info:
+                return title, author
+            if 'Title' in info and info['Title'].strip() != 'untitled':
+                title = info['Title'].strip()
+            if 'Author' in info and info['Author'].strip():
+                author = self._au_last_name(info['Author'].strip())
+            if 'Metadata' in self.doc.catalog:
+                ti, au = self._get_xmp_metadata()
+                if type(ti) is str and ti.strip().lower() != 'untitled':
+                    title = ti
+                if au:
+                    author = au
+        return title, author
+
+    def _get_metadata(self, h):
+        parser = PDFParser(h)
+        try:
+            doc = self.doc = PDFDocument(parser)
+        except PDFSyntaxError:
+            return {}
+        parser.set_document(doc)
+        return doc.info[0]
+
+    def _get_xmp_metadata(self):
+        t = a = None
+        metadata = resolve1(self.doc.catalog['Metadata']).get_data()
+        try:
+            md = xmp_to_dict(metadata)
+        except:
+            return t, a
+        try:
+            t = md['dc']['title']['x-default']
+        except KeyError:
+            pass
+        try:
+            a = md['dc']['creator']
+        except KeyError:
+            pass
+        else:
+            if type(a) is list and len(a) == 1:
+                a = a[0]
+            if type(a) is list:
+                a = '%s %s' % (self._au_last_name(a[0]),
+                        self._au_last_name(a[-1]))
+            elif type(a) is str:
+                a = self._au_last_name(a)
+        return t, a
+
+    def _au_last_name(self, name):
+        return name.split()[-1]
 
 
 if __name__ == "__main__":
-    sys.exit(Foobar(sys.argv[1:]).main())
+    parser = argparse.ArgumentParser(description="PDF batch rename")
+    parser.add_argument('files', nargs='+',
+                        help='list of pdf files to rename')
+    parser.add_argument('-n', dest='dry_run', action='store_true',
+                        help='dry-run listing of filename changes')
+    args = parser.parse_args()
+    sys.exit(RenamePDFsByTitle(args).main())
 
